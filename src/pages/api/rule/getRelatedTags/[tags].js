e58d8b3d9217d8542e1437381ee605bfe43241da
Leider <106984385+leidervinixcode@users.noreply.github.com>
2024-01-29 16:53:14 -0500

    first commit

--- src/pages/api/rule/getRelatedTags/[tags].js
+++ src/pages/api/rule/getRelatedTags/[tags].js
@@ -1,13 +1,15 @@
 import axios from "axios";
 
-export default function GetRelatedTaggs(req, res) {
-    if (req.method === 'GET') {
-        axios.get(
-            `${"https://api.r34.app/booru/rule34.paheal.net/tags?baseEndpoint=rule34.paheal.net"}&tag=${req.query.tags}&order=count&limit=15`
-        ).then((response) => {
-            res.status(200).json(response.data.data);
-        }).catch((error) => {
-            res.status(200).json({ null: true });
-        });
-    }
+export default async function GetRelatedTaggs(req, res) {
+
+    axios.get(
+        `${"https://api.r34.app/booru/rule34.paheal.net/tags?baseEndpoint=rule34.paheal.net"}&tag=${req.query.tags}&order=count&limit=15`
+    ).then((response) => {
+        return res.status(200).json(response.data.data);
+
+    }).catch((error) => {
+        return res.status(200).json({ null: true });
+
+    });
+
 }
